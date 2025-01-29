904bac99b22938532da30a1be6ee617732bb9016
kevsuxdev <markkevinromero.work@gmail.com>
2025-01-29 22:53:44 +0800

    Re-added constant folder to tracking

---
+++ constant/images.ts
@@ -0,0 +1,5 @@
+import getStartedImage from '@/assets/images/getting-start-image.png'
+import facebook from '@/assets/icons/facebook.png'
+import google from '@/assets/icons/google.png'
+
+export default { getStartedImage, facebook, google }
