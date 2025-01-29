fe318e0a52c629832d51e741e745f30f7556b224
kevsuxdev <markkevinromero.work@gmail.com>
2025-01-29 22:51:58 +0800

    Stop tracking constant folder

--- constant/images.ts
+++
@@ -1,5 +0,0 @@
-import getStartedImage from '@/assets/images/getting-start-image.png'
-import facebook from '@/assets/icons/facebook.png'
-import google from '@/assets/icons/google.png'
-
-export default { getStartedImage, facebook, google }
